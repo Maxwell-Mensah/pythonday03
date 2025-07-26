c6c1ab55678b0cbfa40f5401dfe423b516c8086d
unknown <O@Max.afriquetec.com>
2025-07-26 17:27:55 +0200

    ajout d'un nouvel attribut age à la classe Cat

---
+++ ex_04/cat.py
@@ -0,0 +1,42 @@
+class Cat:
+    def __init__(self, age=None, name=None ):
+        self.name=name
+        self.age=age #ajout de l'attribut age a la class Cat
+        if name:
+            print(f"Meow {self.name} !")
+        else:
+            print("Meow")
+            
+    def __del__(self):
+        if self.name:
+            print(f"Bye {self.name} !")
+        else:
+            print("Bye !")
+    
+    def get_name(self):
+        return(self.name)
+    
+    #création d'une méthode pour avoir l'attribut name d'un objet existant
+    def get_age(self):
+        return self.age
+    
+    #création d'une méthode permettant de modifier l'attribut age d'un objet
+    def set_age(self, new_age):
+        self.age=new_age
+    
+    #méthode pemettant de connaître le statut d'un objet en fonction de son attribut age
+    def status(self):
+        match self.age:
+            case a if a==0:
+                print("Unborn Cat")
+            case a if a==1 or a==2:
+                print("Kitten")
+            case a if 3 <= a <= 10:
+                print("Adult Cat")
+            case a if 11 <= a <= 13:
+                print("Old Cat")
+            case _:
+                print("Impossible Cat")
+            
+        
+
