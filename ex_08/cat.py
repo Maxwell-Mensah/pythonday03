149ade925cd0a1cfc971d4b56238944f70dcd3c2
unknown <O@Max.afriquetec.com>
2025-07-27 22:13:41 +0200

    Ajout de la méthode befriend a Cat

---
+++ ex_08/cat.py
@@ -0,0 +1,103 @@
+import random #outils qui permettra de générer aléatoirement un nombre dans un intervalle donné
+
+class Cat:
+    def __init__(self, name=None, age=None, energy=100 ):
+        self.name=name
+        self.age=age
+        self.energy=energy
+        if name:
+            print(f"Meow {self.name} !")
+        else:
+            print("Meow")
+            
+    def __del__(self):
+        if self.name:
+            print(f"Bye {self.name} !")
+        else:
+            print("Bye !")
+    
+    def get_name(self):
+        return(self.name)
+    
+    def get_age(self):
+        return self.age
+
+    def set_age(self, new_age):
+        self.age=new_age
+    
+    def status(self):
+        match self.age:
+            case a if a==0:
+                print("Unborn Cat")
+            case a if a==1 or a==2:
+                print("Kitten")
+            case a if 3 <= a <= 10:
+                print("Adult Cat")
+            case a if 11 <= a <= 13:
+                print("Old Cat")
+            case _:
+                print("Impossible Cat")
+    
+    def hello(self, exp=None):
+        if type(exp)==str:
+            if self.name:
+                print(f"Hello {exp},  I'm {self.name}")
+            else:
+                print(f"Hello {exp}")
+        elif type(exp)==int:
+            if self.name:
+                for i in range (exp):
+                    print(f"Hello, I'm {self.name}")
+            else:
+                for i in range (exp):
+                    print("Hello")
+    
+    def eat(self, food):
+        if food.lower()== "fish":
+            self.energy+=10
+            if self.energy>=100:
+                self.energy=100
+            print("Yum!")
+        elif food.lower()== "vegetable":
+            self.energy-=10
+            if self.energy<=0:
+                self.energy=0
+            print("Yuck!")
+        else:
+            print("I can't eat this")
+            
+    def get_energy(self):
+        return self.energy
+    
+    def set_energy(self, new_energy):
+        self.energy=new_energy
+
+    def nap(self):
+        if self.energy>=25:
+            print("Zzz.....")
+            self.energy-=9
+        else:
+            print("I'm too tired... I need to rest!")
+            self.energy=50
+
+    def befriend(self, other):
+        num=None
+        if type(other)==Cat: 
+            if self.energy<25:
+                print("I'm too tired... I need to rest")
+            else:
+                num=random.randint(1,6) #tirage d'un nombre aléatoirement entre 1 et 6
+                if num==1:
+                    print("I'm too playful for that... hic !")
+                else:
+                    action=random.choice([self.eat(), self.hello()]) #choix d'una action entre eat et hello
+                    action
+        elif type(other)==Dog:
+            if other.energy>=10:
+                other.energy=0
+                print("RUN AWAY !!!")
+            else:
+                print("...")
+        else:
+            print("No way")
+            
